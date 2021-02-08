0e7cf0493c32e17f55f351450b197e8e99de3176
Paul Rep <rep5989@gmail.com>
2021-02-09 00:43:05 +0200

    update settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (env('APP_ENV') !== 'local') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
+
     }
 }
